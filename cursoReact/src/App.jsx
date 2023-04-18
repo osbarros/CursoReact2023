ddd6b9ac77642e2d0b39aa2c24d41324aa60e0c3
osbarros <ceobarros@outlook.com>
2023-04-18 15:33:04 -0300

    configurações iniciais

---
+++ cursoReact/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <div>
+      oi
+      <input>
+      </input>
+      <button
+      onClick={() => alert("Botão apertado com sucesso!")}
+      >
+        Botão teste
+      </button>
+    </div>
+  )
+}
+
+export default App
