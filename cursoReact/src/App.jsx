07b99050454aa90761cdb443045a0cbd9213807e
osbarros <ceobarros@outlook.com>
2023-04-19 09:42:51 -0300

    fix: código antes da aula de rotas

--- cursoReact/src/App.jsx
+++ cursoReact/src/App.jsx
@@ -1,17 +1,46 @@
+import "./App.css";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons"
+import { DivTeste2 } from "./Style";
+import { useState , useEffect} from "react";
 
 function App() {
 
+  const [contador, setContador] = useState(0);
+  const [vetor, setVetor] = useState([]);
+  const [objeto, setObjeto] = useState({})
+
+  useEffect(() => {
+    alert("A variável contador foi alterada para " + contador)
+  }, [contador])
+
   return (
     <div>
-      oi
+      <div className="div-teste">
+        <button> botao 1 </button>
+        <button> botao 2 </button>
+        <button> botao 3 </button>
+      </div>
       <input>
       </input>
-      <button
-      onClick={() => alert("Botão apertado com sucesso!")}
-      >
-        Botão teste
-      </button>
+      <div className="div-botao">
+        <button
+          className="botao-teste"
+          onClick={() => setContador(contador + 1)}
+        >
+          {contador}
+        </button>
+      </div>
+      <div>
+        <Button type="primary" size="large" shape="round" icon={<DownloadOutlined />} className="botao-antd">
+          Baixar arquivo
+        </Button>
+      </div>
+      <DivTeste2>
+        OLÁ, ESSE É O STYLED
+      </DivTeste2>
     </div>
+
   )
 }
 
---
+++ cursoReact/src/Style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+
+export const DivTeste2 = styled.div`
+  width: 50%;
+  height: 80px;
+  margin-top: 40px;
+  border: solid;
+  background-color: green;
+`
